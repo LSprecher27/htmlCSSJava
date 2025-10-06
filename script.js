c8b6d5fc435600acfb0bddc716289aa031530d07
LSprecher27 <mc_LSprecher27@mcjvs2.org>
2025-10-06 10:10:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+//Luke Sprecher
+console.log(`External file loaded!`)
+document.write(`Today I learned how to utilize JavaScript to write lines into the console, change text on my html page using ID inside of tags, and comment inside of JavaScript files. JavaScript, just like CSS, can be integrated inside of html files inside of tags, inside of a <script> tag, lastly referencing js files using **<script src=\"your-javascript-file.js\"></script>** This process was very interesting!`);
